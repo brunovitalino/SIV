c5fc55f249120df18431b143b30908c50a59483b
Bruno Vitalino <brunovitalino@hotmail.com>
2017-03-28 16:49:36 -0300

    Estrutura da classe DFuncionario criada. Método Listar da classe DFuncionario implementado.

---
+++ Dados/Conexao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class Conexao
+    {
+        private static String senha = "";
+        public static String Cn = "Server=localhost; Database=siv; Uid=root; Pwd="+senha+";";
+    }
+}
